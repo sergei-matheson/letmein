82324050dcd5f155e4bb1eecd7435e3caa4ff294
Sergei Matheson <sergei.matheson@gmail.com>
2015-02-18 11:35:59 +1100

    Staging api does not work for outside devs

--- myapp.rb
+++ myapp.rb
@@ -4,9 +4,9 @@ require 'rest_client'
 require 'json'
 
 get '/:number' do
-    resp = RestClient.get "https://staging.api.telstra.com/v1/oauth/token?client_id=[client_id]&client_secret=[secret]&grant_type=client_credentials&scope=SMS"
+    resp = RestClient.get "https://api.telstra.com/v1/oauth/token?client_id=[client_id]&client_secret=[secret]&grant_type=client_credentials&scope=SMS"
     token = JSON.parse(resp)["access_token"]
     header =  {authorization: "Bearer #{token}", "Content-Type" => "application/json"}
-    RestClient.post "https://staging.api.telstra.com/v1/sms/messages", {to: params[:number], body: "Hi Frank, we are at the front door. This has been sent using the Telstra API >:)"}.to_json, header
+    RestClient.post "https://api.telstra.com/v1/sms/messages", {to: params[:number], body: "Hi Frank, we are at the front door. This has been sent using the Telstra API >:)"}.to_json, header
 
 end
